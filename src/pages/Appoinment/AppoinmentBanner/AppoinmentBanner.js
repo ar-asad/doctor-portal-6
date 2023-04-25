a709b11c1c4d7d38c1a05bc9e37c00e68629d179
arasad1 <asaduzzamanaj07@gmail.com>
2023-04-25 08:49:35 +0600

    show all appoinment and service slots in appoinment section

--- src/pages/Appoinment/AppoinmentBanner/AppoinmentBanner.js
+++ src/pages/Appoinment/AppoinmentBanner/AppoinmentBanner.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import chair from '../../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
-// import { DayPicker } from 'react-day-picker';
+import bg from '../../../assets/images/bg.png'
+import 'react-day-picker/dist/style.css';
+
 
 
 const AppoinmentBanner = ({ date, setDate }) => {
     return (
-        <div class="hero min-h-screen">
+        <div style={{
+            background: `url(${bg})`,
+            position: 'center'
+        }} class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse gap-28">
                 <img src={chair} alt='' class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
