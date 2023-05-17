9b0a38322d661652fb584798592c008941a1e0b7
arasad1 <asaduzzamanaj07@gmail.com>
2023-05-17 11:02:05 +0600

    final touch

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -10,7 +10,8 @@ const Banner = () => {
             position: 'center'
         }} className="hero min-h-screen " >
             <div className="hero-content flex-col gap-10 lg:flex-row-reverse " >
-                <img src={chair} className="max-w-sm w-1/2 rounded-lg shadow-2xl" alt='' />
+                {/* banner image */}
+                <img src={chair} className="max-w-sm w-1/2 rounded-lg shadow-2xl" alt='banner' />
                 <div className='w-1/2 '>
                     <h1 className="text-5xl font-bold">Your New Smile Starts Here</h1>
                     <p className="py-6" > Provident cupiditate voluptatem et in.Quaerat fugiat ut assumenda excepturi exercitationem quasi.In deleniti eaque aut repudiandae et a id nisi.</p >
